0f413d3676d518956f1863b224e94b4fb017d1c0
Abhinav <pabhinav1999@gmail.com>
2025-09-08 09:20:50 +0530

    React Episode 1 Akshay Saini

---
+++ Assignments/EP1/App.js
@@ -0,0 +1,21 @@
+const headingRct = React.createElement('h4',{ 'id': 'title'}, 'This is a React Hello world')
+const rootReact = ReactDOM.createRoot(document.getElementById('root'))
+rootReact.render(headingRct)
+
+/*
+ div#parent
+    div#child1
+       h1
+       h1
+    
+    div#child2
+       h2
+       h2
+*/
+
+const nestedRoot = ReactDOM.createRoot(document.getElementById('rootJS'))
+const nestedEle = React.createElement( 'div' , { 'id': 'parent'} , [
+    React.createElement( 'div' , { id : 'child1'} , [React.createElement('h1', "", 'I am a child1 h1 tag1'),React.createElement('h1', "", 'I am a child1 h1 tag2') ]),
+    React.createElement( 'div' , { id : 'child2'} , [React.createElement('h2', "", 'I am a child1 h2 tag1'),React.createElement('h2', "", 'I am a child2 h2 tag2') ])
+])
+nestedRoot.render(nestedEle)
